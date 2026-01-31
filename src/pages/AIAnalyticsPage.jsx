40a87ffe061986edff8db27cfd8d4835b941282f
Azizjon | Frontend Dev <norimboyevazizjon29@gmail.com>
2026-01-31 21:05:24 +0500

    feat: Add AI Analytics page wrapper

---
+++ src/pages/AIAnalyticsPage.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import AIAnalytics from '../components/AIAnalytics/AIAnalytics';
+import './StatsPage.css';
+
+const AIAnalyticsPage = () => {
+  return (
+    <div className="page-container stats-page">
+      <AIAnalytics />
+    </div>
+  );
+};
+
+export default AIAnalyticsPage;
