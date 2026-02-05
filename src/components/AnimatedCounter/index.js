bc55242467d78c68e295e2c463f0636d548d2a45
Azizjon | Frontend Dev <norimboyevazizjon29@gmail.com>
2026-02-05 21:18:12 +0500

    Ishlatilmagan UI komponentlar ochirildi

--- src/components/AnimatedCounter/index.js
+++
@@ -1,6 +0,0 @@
-export { 
-  default, 
-  AnimatedProgress, 
-  AnimatedCircularProgress, 
-  SparkleNumber 
-} from './AnimatedCounter';
--- src/components/Confetti/index.js
+++
@@ -1,1 +0,0 @@
-export { default, useConfetti } from './Confetti';
